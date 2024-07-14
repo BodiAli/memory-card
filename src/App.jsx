03225a0ebf0b860444cf4bf413f175e383f7d86a
Bodi_Ali <bodiali59@gmail.com>
2024-07-14 19:21:32 +0300

    Import necessary components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-// import Header from "./components/Header";
-// import CardsContainer from "./components/CardsContainer";
-// import Footer from "./components/Footer";
+import Header from "./components/Header";
+import CardsContainer from "./components/CardsContainer";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>HIII</h1>
+      <Header />
+      <CardsContainer />
+      <Footer />
     </>
   );
 }
