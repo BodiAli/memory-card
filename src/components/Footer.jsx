fb294693019cd2cf45d088ed6d8c947312ef5709
Bodi_Ali <bodiali59@gmail.com>
2024-07-14 19:23:32 +0300

    Create Footer structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <h2>By Bodi Ali</h2>
+      <div className="github-container">
+        <div className="github"></div>
+        <div className="github-profile"></div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
