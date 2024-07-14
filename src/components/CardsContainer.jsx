74da76ec40edcea8065ec7a5437469cc68ba53cf
Bodi_Ali <bodiali59@gmail.com>
2024-07-14 19:23:09 +0300

    Import Card component

---
+++ src/components/CardsContainer.jsx
@@ -0,0 +1,22 @@
+import Card from "./Card";
+
+function CardsContainer() {
+  return (
+    <main className="main">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </main>
+  );
+}
+
+export default CardsContainer;
