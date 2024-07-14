9c101ea91128a51f5e22ee847c3ef4d45564cf5a
Bodi_Ali <bodiali59@gmail.com>
2024-07-14 19:22:40 +0300

    Add card structure

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+function Card() {
+  return (
+    <div className="card-container">
+      <div className="img"></div>
+      <p className="card-name">Example</p>
+    </div>
+  );
+}
+
+export default Card;
